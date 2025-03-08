8239cc3998f89df2118daa5cb61a977a73554539
Rohit Trivedi <86.rohit@gmail.com>
2025-03-08 19:18:57 +0530

    task creation successful

--- src/routes/index.page.tsx
+++ src/routes/index.page.tsx
@@ -1,9 +1,12 @@
 import { Page } from "rakkasjs";
 import { api } from "../../convex/_generated/api";
 import { useConvexQuery } from "src/useConvexQuery";
+import { useMutation } from "convex/react";
 
 const HomePage: Page = function HomePage() {
   const tasks = useConvexQuery(api.tasks.get);
+  const mutateTask = useMutation(api.tasks.createTask);
+  mutateTask({text: "Hello, world!"});
 
   return (
     <main>
